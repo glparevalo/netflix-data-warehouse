9479e5fbcb4ad7f1fe32122f267db9ee17e3698d
Gabriel Arevalo <lyanearevalo@gmail.com>
2026-02-21 22:56:23 +0800

    create schemas in db

---
+++ scripts/initialize_database.sql
@@ -0,0 +1,10 @@
+-- Create Schemas
+
+-- Bronze Schema
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+-- Silver Schema
+CREATE SCHEMA IF NOT EXISTS silver;
+
+-- Gold Schema
+CREATE SCHEMA IF NOT EXISTS gold;
