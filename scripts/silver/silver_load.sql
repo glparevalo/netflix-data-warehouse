865118535177fcde36f2a8770ecd46c6f428571a
Gabriel Arevalo <lyanearevalo@gmail.com>
2026-02-22 09:44:45 +0800

    updated load silver data

--- scripts/silver/silver_load.sql
+++ scripts/silver/silver_load.sql
@@ -9,21 +9,49 @@ INSERT INTO silver.netflix_data
 					right(show_id, (LENGTH(show_id)-1)), 
 					6, 
 					'0')
-					) as show_key,
+					) AS show_key,
 		show_type,
 		title,
-		director,
-		show_cast,
-		country,
-		date_added,
-		release_year,
-		rating,
-		duration,
-		listed_in,
-		description,
+		COALESCE(
+			TRIM(director),
+			'None Specified'
+		) AS director,
+		COALESCE(
+			TRIM(show_cast),
+			'None Specified'
+		) AS show_cast,
+		TRIM(
+			COALESCE(
+				TRIM(BOTH ',' FROM TRIM(country)), 
+				'None Specified'
+			)
+		) AS country,
+		COALESCE(
+			date_added, 
+			CAST(CONCAT(CAST(release_year AS VARCHAR), '-01-01') AS DATE)
+		) AS date_added,
+		CAST(
+			COALESCE(
+				TRIM(CAST(release_year AS VARCHAR)), 
+				'1900'
+			) AS INT
+		) AS release_year,
+		COALESCE(
+			TRIM(rating),
+			'None Specified'
+		) AS rating,
+		COALESCE(
+			TRIM(duration), 
+			'None Specified'
+		) AS duration,
+		COALESCE(
+			TRIM(listed_in),
+			'None Specified'
+		) AS listed_in,
+		COALESCE(
+			TRIM(description),
+			'None Specified'
+		) AS description,
 		show_id,
 		NOW() as datetime_now
 	FROM bronze.netflix_data;
-
--- validate silver.netflix_data table
-SELECT * FROM silver.netflix_data;
